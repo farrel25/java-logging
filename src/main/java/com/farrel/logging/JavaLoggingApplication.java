ed7453b864a735701bf55490310101456d92b506
Farrel Putra <farrelputradev@gmail.com>
2025-03-13 07:37:17 +0700

    initial commit

---
+++ src/main/java/com/farrel/logging/JavaLoggingApplication.java
@@ -0,0 +1,13 @@
+package com.farrel.logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLoggingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/farrel/logging/JavaLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farrel.logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
